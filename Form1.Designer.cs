cd36178ce9707ed07fd55d305a7e6130b1684f2f
Matthew Delapasse <mdelapasse022721@students.iticollege.edu>
2021-08-26 19:33:01 -0500

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,224 @@
+﻿
+namespace EditingDatabaseRecords
+{
+    partial class frmPhoneDB
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtID = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtNumber = new System.Windows.Forms.TextBox();
+            this.btnFirst = new System.Windows.Forms.Button();
+            this.btnPrevious = new System.Windows.Forms.Button();
+            this.btnNext = new System.Windows.Forms.Button();
+            this.btnLast = new System.Windows.Forms.Button();
+            this.btnEdit = new System.Windows.Forms.Button();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(43, 50);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(42, 15);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Text ID";
+            // 
+            // txtID
+            // 
+            this.txtID.BackColor = System.Drawing.Color.White;
+            this.txtID.Location = new System.Drawing.Point(158, 47);
+            this.txtID.Name = "txtID";
+            this.txtID.Size = new System.Drawing.Size(292, 23);
+            this.txtID.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(43, 88);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(63, 15);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Text Name";
+            // 
+            // txtName
+            // 
+            this.txtName.BackColor = System.Drawing.Color.White;
+            this.txtName.Location = new System.Drawing.Point(158, 85);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(292, 23);
+            this.txtName.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(43, 128);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(75, 15);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Text Number";
+            // 
+            // txtNumber
+            // 
+            this.txtNumber.BackColor = System.Drawing.Color.White;
+            this.txtNumber.Location = new System.Drawing.Point(158, 125);
+            this.txtNumber.Name = "txtNumber";
+            this.txtNumber.Size = new System.Drawing.Size(292, 23);
+            this.txtNumber.TabIndex = 5;
+            // 
+            // btnFirst
+            // 
+            this.btnFirst.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btnFirst.Location = new System.Drawing.Point(85, 269);
+            this.btnFirst.Name = "btnFirst";
+            this.btnFirst.Size = new System.Drawing.Size(81, 27);
+            this.btnFirst.TabIndex = 6;
+            this.btnFirst.Text = "|<";
+            this.btnFirst.UseVisualStyleBackColor = true;
+            this.btnFirst.Click += new System.EventHandler(this.btnFirst_Click);
+            // 
+            // btnPrevious
+            // 
+            this.btnPrevious.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btnPrevious.Location = new System.Drawing.Point(172, 269);
+            this.btnPrevious.Name = "btnPrevious";
+            this.btnPrevious.Size = new System.Drawing.Size(81, 27);
+            this.btnPrevious.TabIndex = 7;
+            this.btnPrevious.Text = "<";
+            this.btnPrevious.UseVisualStyleBackColor = true;
+            this.btnPrevious.Click += new System.EventHandler(this.btnPrevious_Click);
+            // 
+            // btnNext
+            // 
+            this.btnNext.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btnNext.Location = new System.Drawing.Point(259, 269);
+            this.btnNext.Name = "btnNext";
+            this.btnNext.Size = new System.Drawing.Size(81, 27);
+            this.btnNext.TabIndex = 8;
+            this.btnNext.Text = ">";
+            this.btnNext.UseVisualStyleBackColor = true;
+            this.btnNext.Click += new System.EventHandler(this.btnNext_Click);
+            // 
+            // btnLast
+            // 
+            this.btnLast.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btnLast.Location = new System.Drawing.Point(346, 269);
+            this.btnLast.Name = "btnLast";
+            this.btnLast.Size = new System.Drawing.Size(81, 27);
+            this.btnLast.TabIndex = 9;
+            this.btnLast.Text = ">|";
+            this.btnLast.UseVisualStyleBackColor = true;
+            this.btnLast.Click += new System.EventHandler(this.btnLast_Click);
+            // 
+            // btnEdit
+            // 
+            this.btnEdit.Location = new System.Drawing.Point(102, 164);
+            this.btnEdit.Name = "btnEdit";
+            this.btnEdit.Size = new System.Drawing.Size(98, 27);
+            this.btnEdit.TabIndex = 10;
+            this.btnEdit.Text = "Edit";
+            this.btnEdit.UseVisualStyleBackColor = true;
+            this.btnEdit.Click += new System.EventHandler(this.btnEdit_Click);
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(206, 164);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(98, 27);
+            this.btnSave.TabIndex = 11;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.Location = new System.Drawing.Point(310, 164);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(98, 27);
+            this.btnCancel.TabIndex = 12;
+            this.btnCancel.Text = "Cancel";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            this.btnCancel.Click += new System.EventHandler(this.btnCancel_Click);
+            // 
+            // frmPhoneDB
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(484, 322);
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.btnEdit);
+            this.Controls.Add(this.btnLast);
+            this.Controls.Add(this.btnNext);
+            this.Controls.Add(this.btnPrevious);
+            this.Controls.Add(this.btnFirst);
+            this.Controls.Add(this.txtNumber);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtID);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "frmPhoneDB";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Phone DBMS";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.frmPhoneDB_FormClosing);
+            this.Load += new System.EventHandler(this.frmPhoneDB_Load);
+            this.Click += new System.EventHandler(this.btnLast_Click);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        private void FrmPhoneDB_Click(object sender, System.EventArgs e)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtID;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtNumber;
+        private System.Windows.Forms.Button btnFirst;
+        private System.Windows.Forms.Button btnPrevious;
+        private System.Windows.Forms.Button btnNext;
+        private System.Windows.Forms.Button btnLast;
+        private System.Windows.Forms.Button btnEdit;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Button btnCancel;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using System.IO;
+
+namespace EditingDatabaseRecords
+{
+    public partial class frmPhoneDB : Form
+    {
+        public frmPhoneDB()
+        {
+            InitializeComponent();
+        }
+
+        SqlConnection phoneConnecction;
+        SqlCommand phoneCommand;
+        SqlDataAdapter phoneAdapter;
+        DataTable phoneTable;
+        CurrencyManager phoneManager;
+
+        private void frmPhoneDB_Load(object sne3der, EventArgs e)
+        {
+            // get the file path for SQLPhoneDB.mdf
+            string fullPath = Path.GetFullPath("SQLPhoneDB.mdf");
+            // connect to Phone database
+            phoneConnecction = new SqlConnection("Data Source=.\\SQLEXPRESS;AttachDbFilename="+fullPath+";Integrated Security=True; Connect Timeout=30; User Instance=True");
+            phoneConnecction.Open();
+
+            // establish command object
+            phoneCommand = new SqlCommand("SELECT * FROM phonetable ORDER BY ContactName", phoneConnecction);
+
+            // establish data adapter/data table
+            phoneAdapter = new SqlDataAdapter();
+            phoneAdapter.SelectCommand = phoneCommand;
+            phoneTable = new DataTable();
+            phoneAdapter.Fill(phoneTable);
+
+            // bind controls to data table
+            txtID.DataBindings.Add("Text", phoneTable, "ContactID");
+            txtName.DataBindings.Add("Text", phoneTable, "ContactName");
+            txtNumber.DataBindings.Add("Text", phoneTable, "ContactNumber");
+
+            // establish currency manager
+            phoneManager = (CurrencyManager)this.BindingContext[phoneTable];
+
+            SetState("View");
+        }
+
+        private void frmPhoneDB_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            try
+            {
+                // save the updated phone table
+                SqlCommandBuilder phoneAdapterCommands = new SqlCommandBuilder(phoneAdapter);
+                phoneAdapter.Update(phoneTable);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error saving database to file:\r\n" + ex.Message, "Save Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+            // close the connection
+            phoneConnecction.Close();
+            //dispose of the objects
+            phoneConnecction.Dispose();
+            phoneCommand.Dispose();
+            phoneAdapter.Dispose();
+            phoneTable.Dispose();
+        }
+
+        private void btnFirst_Click(object sender, EventArgs e)
+        {
+            phoneManager.Position = 0;
+        }
+
+        private void btnPrevious_Click(object sender, EventArgs e)
+        {
+            phoneManager.Position--;
+        }
+
+        private void btnNext_Click(object sender, EventArgs e)
+        {
+            phoneManager.Position++;
+        }
+        
+        private void btnLast_Click(object sender, EventArgs e)
+        {
+            phoneManager.Position = phoneManager.Count - 1;
+        }
+
+        private void btnEdit_Click(object sender, EventArgs e)
+        {
+            SetState("Edit");
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            phoneManager.EndCurrentEdit();
+            SetState("View");
+            phoneTable.DefaultView.Sort = "ContactName";
+        }
+
+        private void btnCancel_Click(object sender, EventArgs e)
+        {
+            phoneManager.CancelCurrentEdit();
+            SetState("View");
+        }
+
+        private void SetState(string appState)
+        {
+            switch (appState) 
+            {
+                case "View":
+                    btnFirst.Enabled = true;
+                    btnPrevious.Enabled = true;
+                    btnNext.Enabled = true;
+                    btnLast.Enabled = true;
+                    btnEdit.Enabled = true;
+                    btnSave.Enabled = false;
+                    btnCancel.Enabled = false;
+                    txtID.BackColor = Color.White;
+                    txtID.ForeColor = Color.Black;
+                    txtName.ReadOnly = true;
+                    txtNumber.ReadOnly = true;
+                    break;
+                default: // "Edit" mode
+                    btnFirst.Enabled = false;
+                    btnPrevious.Enabled = false;
+                    btnNext.Enabled = false;
+                    btnLast.Enabled = false;
+                    btnEdit.Enabled = false;
+                    btnSave.Enabled = true;
+                    btnCancel.Enabled = true;
+                    txtID.BackColor = Color.Red;
+                    txtID.ForeColor = Color.White;
+                    txtName.ReadOnly = false;
+                    txtNumber.ReadOnly = false;
+                    break;
+            }
+            txtName.Focus();
+        }
+    }
+}
